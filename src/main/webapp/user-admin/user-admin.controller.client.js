fa44716d9756d7337aa47cde2e4171e48d0e43ee
Jake Hansen <hansen.j@husky.neu.edu>
2020-05-13 18:54:05 -0400

    More starting code

--- src/main/webapp/user-admin/user-admin.controller.client.js
+++ src/main/webapp/user-admin/user-admin.controller.client.js
@@ -1,65 +1,66 @@
 (function () {
-  let users = []
-  let $tbody, $addBtn
-  let $usernameFld, $passwordFld, $firstFld, $lastFld, $roleFld
+  var users = [];
+  var $usernameFld, $passwordFld;
+  var $addBtn, $removeBtn, $editBtn, $createBtn;
+  var $usernameFld, $passwordFld, $firstFld, $lastFld, $roleFld;
+  var $userRowTemplate, $tbody;
 
-  function renderAllUsers() {
-    const template = $('.wbdv-user-row-template')[0]
-    const $template = $(template)
-    const clone = $template.clone()
-    console.log($template)
-    $tbody.empty()
-    for(let i=0; i<users.length; i++) {
-      const user = users[i]
-      console.log(user)
-      const copy = clone.clone()
-      copy.find('.wbdv-username').html(user.username)
-      copy.find('.wbdv-first-name').html(user.first)
-      copy.find('.wbdv-last-name').html(user.last)
-      copy.find('.wbdv-role').html(user.role)
-      $tbody.append(copy)
+  var userService = new AdminUserServiceClient();
+
+  $(main);
+
+  function main() {
+    $tbody = $('tbody');
+    $addBtn = $('.wbdv-add-btn');
+    $addBtn.click(createUser);
+    $usernameFld = $('.wbdv-username-fld');
+    $passwordFld = $('.wbdv-password-fld');
+    $firstFld = $('.wbdv-first-fld');
+    $lastFld = $('.wbdv-last-fld');
+    $roleFld = $('.wbdv-role-fld');
+
+    var newTr = $('<tr><td>dan</td></tr>');
+    $tbody.append(newTr);
+
+    for(var i=0; i<users.length; i++) {
+      var username = users[i].username;
+      var newUserRow = $('<tr><td>'+username+'</td></tr>');
+      $tbody.append(newUserRow)
     }
+    renderUsers();
   }
 
   function createUser() {
-    const username = $usernameFld.val()
-    const first = $firstFld.val()
-
-    console.log(username, first)
-    const newUser = {
+    var username = $usernameFld.val();
+    var first = $firstFld.val();
+    var newUser = {
       username: username,
       first: first
-    }
-
-    users.push(newUser)
-    renderAllUsers()
+    };
+    users.push(newUser);
+    renderUsers()
   }
 
-  function main() {
-    $tbody = $('tbody')
-    $addBtn = $('.wbdv-add-btn')
-    $addBtn.css('backgroundColor', 'yellow')
+  function findAllUsers() { … }
+  function findUserById() { … }
+  function deleteUser() { … }
+  function selectUser() { … }
+  function updateUser() { … }
+  function renderUser(user) { … }
 
-    $addBtn.click(createUser)
-
-    $usernameFld = $('.wbdv-username-fld')
-    $passwordFld = $('.wbdv-password-fld')
-    $firstFld = $('.wbdv-first-fld')
-    $lastFld = $('.wbdv-last-fld')
-    $roleFld = $('.wbdv-role-fld')
-
-    const newTr = $('<tr><td>dan</td></tr>')
-    $tbody.append(newTr)
-
-    for(let i=0; i<users.length; i++) {
-      const username = users[i].username
-      const newUserRow = $('<tr><td>'+username+'</td></tr>')
-      $tbody.append(newUserRow)
+  function renderUsers(users) {
+    var template = $('.wbdv-user-row-template')[0];
+    var $template = $(template);
+    var clone = $template.clone();
+    $tbody.empty();
+    for(var i=0; i<users.length; i++) {
+      var user = users[i];
+      var copy = clone.clone()
+      copy.find('.wbdv-username').html(user.username);
+      copy.find('.wbdv-first-name').html(user.first);
+      copy.find('.wbdv-last-name').html(user.last);
+      copy.find('.wbdv-role').html(user.role);
+      $tbody.append(copy)
     }
-
-    renderAllUsers()
   }
-
-  jQuery(main)
-
-})()
+})();
