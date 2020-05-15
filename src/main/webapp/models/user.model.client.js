66152abd118da5ff4eadd88fb9ed6d448b6c3cca
Jake Hansen <hansen.j@husky.neu.edu>
2020-05-14 21:21:09 -0400

    Finishes user model

--- src/main/webapp/models/user.model.client.js
+++ src/main/webapp/models/user.model.client.js
@@ -1,17 +1,95 @@
-function User(username, password, firstName, lastName, role) {
+function User(username, password, email, firstName, lastName, phone, role, dob) {
+  // Constants
   this.username = username;
   this.password = password;
-  // ...same for rest of properties…
+  this.email = email;
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.phone = phone;
+  this.role = role;
+  this.dob = dob;
+
+  // Bindings
+  this.getUsername = getUsername;
+  this.getPassword = getPassword;
+  this.getEmail = getEmail;
+  this.getFirstName = getFirstName;
+  this.getLastName = getLastName;
+  this.getPhone = getPhone;
+  this.getRole = getRole;
+  this.getDoB = getDoB;
 
   this.setUsername = setUsername;
-  this.getUsername = getUsername;
-  // ...same for rest of properties…
+  this.setPassword = setPassword;
+  this.setEmail = setEmail;
+  this.setFirstName = setFirstName;
+  this.setLastName = setLastName;
+  this.setPhone = setPhone;
+  this.setRole = setRole;
+  this.setDoB = setDoB;
 
-  function setUsername(username) {
-    this.username = username;
-  }
+  // Getters
   function getUsername() {
     return this.username;
   }
-  // ...same for rest of properties…
+
+  function getPassword() {
+    return this.password;
+  }
+
+  function getEmail() {
+    return this.email;
+  }
+
+  function getFirstName() {
+    return this.firstName;
+  }
+
+  function getLastName() {
+    return this.lastName;
+  }
+
+  function getPhone() {
+    return this.phone;
+  }
+
+  function getRole() {
+    return this.role;
+  }
+
+  function getDoB() {
+    return this.dob;
+  }
+
+  // Setters
+  function setUsername(username) {
+    this.username = username;
+  }
+  function setPassword(username) {
+    this.username = username;
+  }
+
+  function setEmail(email) {
+    this.email = email;
+  }
+
+  function setFirstName(firstName) {
+    this.firstName = firstName;
+  }
+
+  function setLastName(lastName) {
+    this.lastName = lastName;
+  }
+
+  function setPhone(phone) {
+    this.phone = phone;
+  }
+
+  function setRole(role) {
+    this.role = role;
+  }
+
+  function setDoB(dob) {
+    this.dob = dob;
+  }
 }
