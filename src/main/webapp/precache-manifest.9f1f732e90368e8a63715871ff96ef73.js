a417ece6144954a041f2edd73ffe5792d26aefc5
Jake Hansen <hansen.j@husky.neu.edu>
2020-06-14 00:20:51 -0400

    Copied client build files

---
+++ src/main/webapp/precache-manifest.9f1f732e90368e8a63715871ff96ef73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35f595e7ede9ce4e9b0a13525b2937ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef36ae08ff9135b01a47",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8d6f2646e092073a6f55",
+    "url": "/static/css/main.0534b0a5.chunk.css"
+  },
+  {
+    "revision": "ef36ae08ff9135b01a47",
+    "url": "/static/js/2.f4d2cf3d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f4d2cf3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d6f2646e092073a6f55",
+    "url": "/static/js/main.6e096509.chunk.js"
+  },
+  {
+    "revision": "28e26d5c56b235ae6781",
+    "url": "/static/js/runtime-main.50e1bfd1.js"
+  }
+]);
--- src/main/webapp/service-worker.js
+++ src/main/webapp/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a5908844e24349ddbfc9425e32f2bc57.js"
+  "/precache-manifest.9f1f732e90368e8a63715871ff96ef73.js"
 );
 
 self.addEventListener('message', (event) => {
